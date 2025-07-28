308f4ee8984ce10265507790977e487205b94893
João Victor <skai7ami@gmail.com>
2025-07-28 08:52:50 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args) { }
+    private static void Main(string[] args)
+    {
+        var solution = new Solution();
+
+        Console.WriteLine(solution.ContainsDuplicate([1, 2, 3, 1]));
+    }
 }
 
-public class Solution { }
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var temp = new HashSet<int>();
+        foreach (var n in nums)
+        {
+            if (temp.Contains(n))
+                return true;
+            temp.Add(n);
+        }
+        return false;
+    }
+}
