58578228e946bfe858d1e0ffa07d7718cbd0d749
João Victor <skai7ami@gmail.com>
2025-07-29 11:34:02 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -3,7 +3,37 @@
     private static void Main(string[] args)
     {
         var solution = new Solution();
+        System.Console.WriteLine(solution.LongestCommonPrefix(["flower", "flow", "flight"]));
+        System.Console.WriteLine(solution.LongestCommonPrefix(["dog", "racecar", "car"]));
     }
 }
 
-public class Solution { }
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var minor = strs[0];
+        if (minor == string.Empty)
+            return minor;
+        var common = string.Empty;
+
+        foreach (var s in strs)
+        {
+            if (s.Length < minor.Length)
+                minor = s;
+        }
+
+        foreach (var c in minor)
+        {
+            common += c;
+
+            foreach (var s in strs)
+            {
+                if (!s.StartsWith(common))
+                    return common[..^1];
+            }
+        }
+
+        return common;
+    }
+}
