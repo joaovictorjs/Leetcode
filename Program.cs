6f44ad20da0d4eb261dd6671867f7ed018fea78a
joaovictorjs <skai7ami@gmail.com>
2025-07-28 23:13:01 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -3,7 +3,24 @@
     private static void Main(string[] args)
     {
         var solution = new Solution();
+        var result = solution.Intersection([1, 2, 2, 1], [2, 2]);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
 
-public class Solution { }
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var intersection = Array.Empty<int>();
+        foreach (var item in nums1)
+        {
+            if (nums2.Contains(item) && !intersection.Contains(item))
+                intersection = [.. intersection, item];
+        }
+        return intersection;
+    }
+}
