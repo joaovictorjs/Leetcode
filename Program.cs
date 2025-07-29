e4052df7cff4dfadde88826d0f899123250ebef5
João Victor <skai7ami@gmail.com>
2025-07-29 07:39:42 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -3,7 +3,24 @@
     private static void Main(string[] args)
     {
         var solution = new Solution();
+
+        Console.WriteLine(solution.MissingNumber([3, 0, 1]));
     }
 }
 
-public class Solution { }
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        var length = nums.Length;
+        var sum = nums.Sum();
+        static int consecutiveSum(int accumulator, int index, int target)
+        {
+            if (index > target)
+                return accumulator;
+            return consecutiveSum(accumulator += index++, index, target);
+        }
+        var expectedSum = consecutiveSum(0, 0, length);
+        return expectedSum - sum;
+    }
+}
