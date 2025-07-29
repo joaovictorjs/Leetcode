b43f545ea3898ede3d6ae5e22c7436c44c87137d
João Victor <skai7ami@gmail.com>
2025-07-29 08:06:48 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -3,7 +3,36 @@
     private static void Main(string[] args)
     {
         var solution = new Solution();
+        System.Console.WriteLine(solution.WordPattern("abba", "dog cat cat dog"));
+        System.Console.WriteLine(solution.WordPattern("aba", "dog cat cat"));
     }
 }
 
-public class Solution { }
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        var letters = pattern.ToCharArray();
+        var words = s.Split(" ");
+        var dict = new Dictionary<char, string>();
+        var phrase = string.Empty;
+        var lettersSet = new HashSet<char>();
+        var wordsSet = new HashSet<string>();
+
+        if (words.Length != letters.Length)
+            return false;
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            var l = letters[i];
+            var w = words[i];
+            if (!dict.ContainsKey(l))
+                dict.Add(l, w);
+            phrase += $" {dict[l]}";
+            lettersSet.Add(l);
+            wordsSet.Add(w);
+        }
+
+        return s == phrase.Trim() && wordsSet.Count == lettersSet.Count;
+    }
+}
